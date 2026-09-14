ca73e6b1df170cfa579417c86e1a437aecd09a15
Ron <crypticfn2012@gmail.com>
2026-09-14 22:52:41 +0100

    Load profile styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import './styles.css';
+import './profile.css';
 import './gameLaunchBridge';
 import App from './App';
 
